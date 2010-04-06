7e847880238e0f206e045a1f732d4d2d18d7e2a4
Ryan Pavlik <rpavlik@iastate.edu>
2010-04-06 07:43:59 -0500

    Complete the flagpoll formula

--- Library/Formula/flagpoll.rb
+++ Library/Formula/flagpoll.rb
@@ -2,10 +2,8 @@ require 'formula'
 
 class Flagpoll <Formula
   url 'http://flagpoll.googlecode.com/files/flagpoll-0.9.1.tar.gz'
-  homepage ''
-  md5 ''
-
-# depends_on 'cmake'
+  homepage 'http://code.google.com/p/flagpoll/'
+  md5 'c4ac50ae99a880704abfc62a64ed16aa'
 
   def install
     system "./setup.py", "install", "--prefix=#{prefix}"
