2afec09cc087e94bfbf15778c67cde45eff27e08
Ryan Pavlik <rpavlik@iastate.edu>
2010-04-13 10:25:19 -0400

    Add cppcheck formula

---
+++ Library/Formula/cppcheck.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Cppcheck <Formula
+  head 'git://github.com/danmar/cppcheck.git'
+  homepage 'http://cppcheck.wiki.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/cppcheck/cppcheck/1.42/cppcheck-1.42.tar.bz2'
+  md5 '20c495b191a9f4fdd030656ad9d4741d'
+
+  def install
+    system "make", "install", "BIN=#{prefix}/bin"
+  end
+end
