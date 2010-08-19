6968492156289953e03a3dce7dfd909496b00f4c
Ryan Pavlik <rpavlik@iastate.edu>
2010-08-19 13:22:54 -0500

    Improve/stabilize the vrpn formula

--- Library/Formula/vrpn.rb
+++ Library/Formula/vrpn.rb
@@ -1,17 +1,20 @@
 require 'formula'
 
 class Vrpn <Formula
-  #url 
-  head 'git://github.com/rpavlik/vrpn.git'
-  #version '07.26.2'
+  # We'll use this again when the below commit is merged
+  #url 'git://git.cs.unc.edu/vrpn.git', :revison => ''
+  url 'git://github.com/rpavlik/vrpn.git', :revision => '03fada7a988f7e182f16'
+  version '07.26.2'
+  head 'git://git.cs.unc.edu/vrpn.git'
   homepage 'http://vrpn.org'
 
   depends_on 'cmake'
 
   def install
-    system "mkdir build"
-    Dir.chdir "build"
-    system "cmake .. #{std_cmake_parameters}"
-    system "make install"
+    Dir.mkdir "build"
+    Dir.chdir "build" do
+	  system "cmake .. #{std_cmake_parameters}"
+      system "make install"
+	end
   end
 end
