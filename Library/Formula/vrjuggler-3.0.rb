3d58ea2a4d99df9f34ce414aa4bf1d1d095cad16
Ryan Pavlik <rpavlik@iastate.edu>
2010-08-27 17:32:09 -0500

    Remove extra spaces and tabs from juggler brews

--- Library/Formula/vrjuggler-3.0.rb
+++ Library/Formula/vrjuggler-3.0.rb
@@ -16,40 +16,40 @@ class Vrjuggler30 <Formula
       "--with-boost=#{HOMEBREW_PREFIX}",
       "--with-alut=#{HOMEBREW_PREFIX}",
       "--with-vrpn=#{HOMEBREW_PREFIX}"]
-    
+
     # For some reason, juggler fails to build nicely in parallel in any kind
     # of packinging-like setup
     ENV.deparallelize()
-    
+
     # Make our local aclocal dir before autogen, to be safe and avoid errors
     system "mkdir -p #{prefix}/share/aclocal"
-    
+
     ENV['ACLOCAL_FLAGS'] = "-I #{prefix}/share/aclocal -I #{HOMEBREW_PREFIX}/share/aclocal"
     ENV['FLAGPOLL_PATH'] = "#{prefix}/lib/flagpoll:#{prefix}/share/flagpoll"
     ENV['AUTOCONF'] = "autoconf"
     ENV['AUTOHEADER'] = "autoheader"
     ENV['ACLOCAL'] = "aclocal-1.10"
     if not ARGV.include? '--use-llvm'
-	    ENV['CC'] = "gcc"
-	    ENV['CXX'] = "g++"
-	end
+      ENV['CC'] = "gcc"
+      ENV['CXX'] = "g++"
+    end
 
     # Make the default Java location correct
-    inreplace 'modules/tweek/java/tweek-base.sh.in' do |contents| 
+    inreplace 'modules/tweek/java/tweek-base.sh.in' do |contents|
       contents.gsub! /\/usr\/java/, '/usr'
     end
 
     system "./autogen.sh"
-    
+
     # The prefix set here is immediately written to, so using the keg.
     system "./configure.pl", *args
-    
+
     # setting the instprefix variable to put the homebrew prefix as the
     # libraries' "known root dir", while installing to the keg.
 
     # Make only the optimized shared libraries
     system "make", "opt-dso", "instprefix=#{HOMEBREW_PREFIX}"
-    
+
     # Install all available optimized libraries - in this case, only
     # shared available, static are peacefully ignored.
     system "make", "install-optim", "instprefix=#{HOMEBREW_PREFIX}"
