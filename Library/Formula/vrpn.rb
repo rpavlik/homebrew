ee0e407ad91be9bfd946cadcad5139c0811d292f
Ryan Pavlik <rpavlik@iastate.edu>
2010-09-13 11:31:57 -0500

    Update VRPN to newer commit

--- Library/Formula/vrpn.rb
+++ Library/Formula/vrpn.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Vrpn <Formula
   # We'll use this again when the below commit is merged
-  #url 'git://git.cs.unc.edu/vrpn.git', :revison => ''
-  url 'git://github.com/rpavlik/vrpn.git', :revision => '03fada7a988f7e182f16'
-  version '07.26.2'
+  url 'git://git.cs.unc.edu/vrpn.git', :revison => 'a91a057cb7fee27b482b3c8253d0c4e24a96817f'
+  #url 'git://github.com/rpavlik/vrpn.git', :revision => '03fada7a988f7e182f16'
+  version '07.26.3'
   head 'git://git.cs.unc.edu/vrpn.git'
   homepage 'http://vrpn.org'
 
