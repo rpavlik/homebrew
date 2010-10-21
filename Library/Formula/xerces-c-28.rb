1742b375cab95f025f95d94f8ac061043e2148b2
Eric Marsh <farmdevil@cox.net>
2010-10-21 13:34:37 -0500

    It almost works!

--- Library/Formula/xerces-c-28.rb
+++ Library/Formula/xerces-c-28.rb
@@ -1,12 +1,28 @@
 require 'formula'
 
-class XercesC <Formula
+class XercesC28 <Formula
   url 'http://mirror.olnevhost.net/pub/apache//xerces/c/2/sources/xerces-c-src_2_8_0.tar.gz'
   homepage 'http://xerces.apache.org/xerces-c/'
   md5 '5daf514b73f3e0de9e3fce704387c0d2'
+  
+  def patches
+    {:p0 => "http://svn.macports.org/repository/macports/trunk/dports/textproc/xercesc/files/64-bit-no-carbon.diff"}
+  end
+
 
   def install
-    system "./runConfigure","-pmacosx","-P#{prefix}","-nnative"
-    system "make install"
+	srcdir = Dir.getwd + "/src"
+	Dir.chdir 'src/xercesc' do
+	  ENV["XERCESCROOT"] = "#{prefix}"
+	  ENV.deparallelize
+	  args = ["-pmacosx","-P", "#{prefix}","-nnative", "-z", "-I#{srcdir}"]
+	  if snow_leopard_64?
+		args << "-b"
+		args << "64"
+	  end
+	  system "./runConfigure", *args
+	  system "make"
+      system "make install"
+	end
   end
 end
