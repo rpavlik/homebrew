1938cd69b00aa99056c98e043c26ebb4fa163d7f
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-04 11:16:53 -0600

    Add a head source for gmtl

--- Library/Formula/gmtl.rb
+++ Library/Formula/gmtl.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Gmtl <Formula
   url 'http://downloads.sourceforge.net/project/ggt/Generic%20Math%20Template%20Library/0.6.0/gmtl-0.6.0.tar.gz'
+  head 'https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/'
   homepage 'http://ggt.sourceforge.net/'
   md5 '018c2cce3c87ad63509481b1eb144387'
 
