6efc0747d90cb3ec96ed32fcd6b8d1554e66d343
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-04 11:27:53 -0600

    Upgrade GMTL to 0.6.1

--- Library/Formula/gmtl.rb
+++ Library/Formula/gmtl.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Gmtl <Formula
-  url 'http://downloads.sourceforge.net/project/ggt/Generic%20Math%20Template%20Library/0.6.0/gmtl-0.6.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/ggt/Generic%20Math%20Template%20Library/0.6.1/gmtl-0.6.1.tar.gz'
+  md5 '1391af2c5ea050dda7735855ea5bb4c1'
   head 'https://ggt.svn.sourceforge.net/svnroot/ggt/trunk/'
   homepage 'http://ggt.sourceforge.net/'
-  md5 '018c2cce3c87ad63509481b1eb144387'
 
   depends_on 'scons' => :build
   def patches
